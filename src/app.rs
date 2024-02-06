cd1ba112c1e67661187be5d7b2081f0adac6a891
sofa <squishysparrow7.7@gmail.com>
2024-02-06 17:40:54 +1300

    Move the no-javascript script to the index.html for faster response time
    When on a slow connection, the wasm won't load fast enough to delete the
    message before it shows up. By putting it on the index.html the script
    will be loaded at the same time as the div

--- src/app.rs
+++ src/app.rs
@@ -4,10 +4,6 @@ use leptos_router::*;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <script>
-            // Get rid of the no-javascript message, won't need it.
-            document.getElementById("no-javascript").remove();
-        </script>
         <Router fallback=|| view! { <p>"This page doesn't exist :("</p> }.into_view()>
             <h1>this-doesnt.work</h1>
             <Routes>
